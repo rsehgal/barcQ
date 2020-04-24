e0ea1f60e6b98d32cc53ea360573b092add4378b
Raman Sehgal <sc.ramansehgal@gmail.com>
2020-04-24 20:53:51 +0530

    Now on dropping a gate to a grid of div, automatically change the div attributes on DOM which is required to convert them into the JSON. Similarly upon deletion of a gate, automatically resets the parent div attributes.

--- frontend/js/toolbox.js
+++ frontend/js/toolbox.js
@@ -1,6 +1,7 @@
 function CreateToolBox(){
 
-var mydata = JSON.parse(data);
+var mydataExtended = JSON.parse(gateJson);
+var mydata = mydataExtended.instructions;
 var numOfRows=mydata.length;
 var $table = $('<table border=1px/>');
 var index=0;
@@ -10,7 +11,17 @@ for(var index=0; index<parseInt(numOfRows); index++){
   //console.log(gate1JustName);
   var gate1=gate1JustName+".png";
   //console.log(gate1);
-  var rowStr='<td><img src="../../images/'+gate1+'" id='+gate1JustName+ ' class="cloneAble"/></td>';
+  var rowStr='<td><img src="../../images/'+gate1+'" id='
+                      +gate1JustName
+                      +' class="cloneAble" gate="'
+                      +gate1JustName
+                      +'" num_bits='+mydata[index].num_bits
+                      +' ctl_enabled='+mydata[index].ctl_enabled
+                      +' ctl_bits='+mydata[index].ctl_bits
+                      +' tgt_bits='+mydata[index].tgt_bits
+                      +' arg_enabled='+mydata[index].arg_enabled
+                      +' arg_value='+mydata[index].arg_value
+                      +' /></td>';
   $row.append(rowStr);
   $table.append($row);
   
