b1a8f6c337b077b0b024f2becca00d1001b610dc
Raman Sehgal <sc.ramansehgal@gmail.com>
2020-05-31 22:22:23 +0530

    [Issue-15]; Added a demo to link the frontend with cherryPy

---
+++ backend/cherryPyWork/pythonBackend.py
@@ -0,0 +1,63 @@
+import os, os.path
+import random
+import string
+
+import cherrypy
+
+#MEDIA_DIR = os.path.join(os.path.abspath("."), u"media")
+
+class StringGenerator(object):
+    @cherrypy.expose
+    def index(self):
+        #return open('public/test.html')
+        return open('../../frontend/GridInterface/samples/gridSkeleton.html')
+
+    @cherrypy.expose
+    def generate(self, length=8):
+        some_string = ''.join(random.sample(string.hexdigits, int(length)))
+        cherrypy.session['mystring'] = some_string
+        return some_string
+
+    @cherrypy.expose
+    def display(self):
+        return cherrypy.session['mystring']
+
+if __name__ == '__main__':
+     conf = {
+
+         '/': {
+             'tools.sessions.on': True,
+             'tools.staticdir.root': os.path.abspath(os.getcwd())
+         },
+         '/static': {
+             'tools.staticdir.on': True,
+             #'tools.staticdir.dir': './public'
+             #'tools.staticdir.dir': '/home/rsehgal/barcQ/frontend'
+             'tools.staticdir.dir': '../../frontend'
+         },
+         '/images': {
+             'tools.staticdir.on': True,
+             #'tools.staticdir.dir': './public'
+             #'tools.staticdir.dir': '/home/rsehgal/barcQ/frontend/images'
+             'tools.staticdir.dir': '../../frontend/images'
+         },
+         '/css': {
+             'tools.staticdir.on': True,
+             #'tools.staticdir.dir': './public'
+             #'tools.staticdir.dir': '/home/rsehgal/barcQ/frontend/css'
+             'tools.staticdir.dir': '../../frontend/css'
+         },
+         '/js': {
+             'tools.staticdir.on': True,
+             #'tools.staticdir.dir': './public'
+             #'tools.staticdir.dir': '/home/rsehgal/barcQ/frontend/js'
+             'tools.staticdir.dir': '../../frontend/js'
+         },
+         '/jsonschemas': {
+             'tools.staticdir.on': True,
+             #'tools.staticdir.dir': './public'
+             #'tools.staticdir.dir': '/home/rsehgal/barcQ/frontend/jsonschemas'
+             'tools.staticdir.dir': '../../frontend/jsonschemas'
+         }
+     }
+     cherrypy.quickstart(StringGenerator(), '/', conf)
