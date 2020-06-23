3c5d71197c10c6d3296e7082a00473c2343f8407
Raman Sehgal <sc.ramansehgal@gmail.com>
2020-06-23 15:30:33 +0530

    [QFT] :  Added 2 qubit QFT as a gate in the portal.

--- backend/jsonClass.py
+++ backend/jsonClass.py
@@ -122,17 +122,42 @@ class CircuitCreator:
 		idenListEnd = []
 		for idenIndex in range(targetStartIndex):
 			idenListBeg.append(qeye(2))
-		for idenIndex in range(targetEndIndex,self.N):
+		#print("@@@@@@@@@@@@@ IDEN_LIST_BEG @@@@@@@@@@@")
+		#print(idenListBeg)
+		for idenIndex in range(targetEndIndex+1,self.N):
 			idenListEnd.append(qeye(2))
-		
+		#print("@@@@@@@@@@@@@ IDEN_LIST_END @@@@@@@@@@@")
+		#print(idenListEnd)
+		#qftMatrix = qft(len(targets))
 		finalMatrix = qft(len(targets))
-		'''
+		#print("@@@@@@@@@@@@@ QFTMatrix @@@@@@@@@@@")
+		#print(qftMatrix)
+
+		#print("Length of idenListBeg : "+str(len(idenListBeg)))
+		startingTensor=qeye(2)
+		#finalMatrix=0
 		if len(idenListBeg) != 0:
-			finalMatrix=tensor(idenListBeg,finalMatrix)
-		if len(idenListEnd) != 0:
-			finalMatrix=tensor(finalMatrix,idenListEnd)
-		'''
 			
+			for index in range(1,len(idenListBeg)):
+				startingTensor = tensor(startingTensor,qeye(2))
+			finalMatrix = tensor(startingTensor,finalMatrix)
+		#print("@@@@@@@@@@@@@ startingTensor @@@@@@@@@@@")
+		#print(startingTensor)
+
+		#print("Length of idenListEnd : "+str(len(idenListEnd)))
+		endingTensor=qeye(2)
+		if len(idenListEnd) != 0:
+			
+			for index in range(1,len(idenListEnd)):
+				endingTensor = tensor(endingTensor,qeye(2))
+			finalMatrix = tensor(finalMatrix,endingTensor)
+		#print("@@@@@@@@@@@@@ endingTensor @@@@@@@@@@@")
+		#print(endingTensor)
+
+		#finalMatrix = tensor(startingTensor,qftMatrix,endingTensor)
+		#print("@@@@@@@@@@@@@ finalMatrix @@@@@@@@@@@")
+		#print(finalMatrix)
+
 		return finalMatrix
 		
         
