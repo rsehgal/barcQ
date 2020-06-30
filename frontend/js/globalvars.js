a25f232b66a8a8d69d2dcb1a33e9e2fafd87ce97
Raman Sehgal <sc.ramansehgal@gmail.com>
2020-06-30 17:45:00 +0530

    [Issue-65]; Introduced a better scheme for user defined control gates, demonstrated for CNOT.

--- frontend/js/globalvars.js
+++ frontend/js/globalvars.js
@@ -18,8 +18,8 @@ var rowIds=[];
 var uniqueSortedRowIds=[];
 var gridConnectorRadius=3;
 var userControlGateFlag=0;
-var singleDivWidth=35;
-var singleDivHeight=35;
+var singleDivWidth=36;
+var singleDivHeight=36;
 var xorrad=10;
 var controlrad=6;
 
@@ -92,3 +92,7 @@ var countData=[{
 
 //Variable introduced for GenericControllUnitaryGates
 var colIdOfGenericControlledUnitaryGate=0;
+var targetUnitaryGate="";
+var unitaryControlRowIds=[];
+var unitaryTargetRowIds=[];
+var offSetRowId=0;
