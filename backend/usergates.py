ec6e98e7ddccb4bfea3e93e07cd232489d49e28b
Raman Sehgal <sc.ramansehgal@gmail.com>
2020-07-05 18:13:36 +0530

    [Issue-65]; Added a mechanism to expand a gate from n to N bits.             This will be useful for all the gate having multiple inputs. 	    Added QFT and IQFT in a more generic way using this mechanism. 	    Using this mechanism, any new gate can be injected in the system.

--- backend/usergates.py
+++ backend/usergates.py
@@ -17,6 +17,53 @@ def z(N=None, target=0):
 	else:
 		return sigmaz()
 
+def gate_expand_ntoN(mat,targets,N):
+	targetStartIndex = targets[0]
+	targetEndIndex = targets[len(targets)-1]
+	idenListBeg = []
+	idenListEnd = []
+	for idenIndex in range(targetStartIndex):
+		idenListBeg.append(qeye(2))
+	#print("@@@@@@@@@@@@@ IDEN_LIST_BEG @@@@@@@@@@@")
+	#print(idenListBeg)
+	for idenIndex in range(targetEndIndex+1,N):
+		idenListEnd.append(qeye(2))
+	#print("@@@@@@@@@@@@@ IDEN_LIST_END @@@@@@@@@@@")
+	#print(idenListEnd)
+	#qftMatrix = qft(len(targets))
+		
+	#finalMatrix = qft(len(targets))
+	finalMatrix = mat
+		
+	#print("@@@@@@@@@@@@@ QFTMatrix @@@@@@@@@@@")
+	#print(qftMatrix)
+
+	#print("Length of idenListBeg : "+str(len(idenListBeg)))
+	startingTensor=qeye(2)
+	#finalMatrix=0
+	if len(idenListBeg) != 0:
+			
+		for index in range(1,len(idenListBeg)):
+			startingTensor = tensor(startingTensor,qeye(2))
+		finalMatrix = tensor(startingTensor,finalMatrix)
+	#print("@@@@@@@@@@@@@ startingTensor @@@@@@@@@@@")
+	#print(startingTensor)
+
+	#print("Length of idenListEnd : "+str(len(idenListEnd)))
+	endingTensor=qeye(2)
+	if len(idenListEnd) != 0:
+			
+		for index in range(1,len(idenListEnd)):
+			endingTensor = tensor(endingTensor,qeye(2))
+		finalMatrix = tensor(finalMatrix,endingTensor)
+	#print("@@@@@@@@@@@@@ endingTensor @@@@@@@@@@@")
+	#print(endingTensor)
+
+	#finalMatrix = tensor(startingTensor,qftMatrix,endingTensor)
+	#print("@@@@@@@@@@@@@ finalMatrix @@@@@@@@@@@")
+	#rint(finalMatrix)
+
+	return finalMatrix
 
 def main():
 	print(x(N=1,target=0))
