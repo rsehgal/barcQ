df67c5d151fd0915de555cacbdbc5e03e2077175
rsehgal <sc.ramansehgal@gmail.com>
2020-07-10 17:33:37 +0530

    Revert "[Issue-65]; Now able to set the Phase value using popup textbox."
    This reverts commit de8fb6168712c90de2fc0fa1e23894bf7571f987.

--- frontend/js/mergeandunmerge.js
+++ frontend/js/mergeandunmerge.js
@@ -400,18 +400,6 @@ function InsertCell(rownum, colnum){
   	AttachDroppableEvents();
 }
 
-/*
- * Code for triggering phase popup
- */
-function AttachPhasePopup(obj){
-	//gateObj=obj;
-	obj.on("contextmenu", function(event) {
-		gateObj=$(this);
-		event.preventDefault();
-		$('#phasePopup').show();
-	});
-}
-
 
 /**
  * Code for Generic controlled Unitary gates
@@ -427,8 +415,7 @@ function AttachGenericControlPopup(obj){
 		//alert("Right clicked from Attach Generic .........");
 		//stop showing browser menu
 		event.preventDefault();
-		//$('.hover_bkgr_fricc').show();
-		$('#gatePopup').show();
+		$('.hover_bkgr_fricc').show();
 		colIdOfGenericControlledUnitaryGate = parseInt(obj.parent().attr("columnid"));
 	    //alert(columnId);
 	});
